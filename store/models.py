197597cda2c4f3a59ebbcddbb1568ff0af052184
manchur <manchur.fiverr@gmail.com>
2023-04-09 20:01:11 +0600

    add some thing

--- store/models.py
+++ store/models.py
@@ -32,7 +32,7 @@ class Customer(models.Model):
         (MEMBERSHIP_SILVER, "Silver"),
         (MEMBERSHIP_GOLD, "Gold")
     ]
-    givem_name=models.CharField(max_length=255)
+    first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     email=models.EmailField(unique=True)
     phone=models.CharField(max_length=255)
